69a91d440a2aa1ac9f05fbb3827e45d84997ba3c
NahidKakon <nahidshakib@outlook.com>
2024-11-02 20:00:03 +0000

    Update Name.jsx

--- app/components/Name.jsx
+++ app/components/Name.jsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image';
 
-// function Name() {
-//   return (
-//     <div>
-//          <Image
+function Name() {
+   return (
+     <div>
+          <Image
           
-//           src="/name.svg"
-//           alt="logo"
-//           className="transition-transform duration-300 cursor-pointer hover:-rotate-6"
-//           width={200}
-//           height={150}
+           src="/name.svg"
+           alt="logo"
+           className="transition-transform duration-300 cursor-pointer hover:-rotate-6"
+           width={200}
+           height={150}
         
-//         />
+         />
       
-//     </div>
+     </div>
   )
 }
 export default Name;
