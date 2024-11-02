96b1eefd36018783f5dde619745af704d9101dcf
NahidKakon <173110433+NahidKakon@users.noreply.github.com>
2024-11-02 13:39:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/components/Name.jsx
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import Image from 'next/image';
+
+// function Name() {
+//   return (
+//     <div>
+//          <Image
+          
+//           src="/name.svg"
+//           alt="logo"
+//           className="transition-transform duration-300 cursor-pointer hover:-rotate-6"
+//           width={200}
+//           height={150}
+        
+//         />
+      
+//     </div>
+  )
+}
+export default Name;
